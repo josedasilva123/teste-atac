dff900e75bab42ede87b3260ca6cc2e0aba9731a
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-15 16:40:08 -0300

    chore: adding missing x and y in create costumer repository

--- backend/src/repositories/costumers/create.repository.ts
+++ backend/src/repositories/costumers/create.repository.ts
@@ -2,13 +2,13 @@ import { client } from "../../database/client";
 import { ICostumer, TCreateCostumerData } from "../../interfaces/costumers.interface";
 
 export const create = async (body: TCreateCostumerData) => {
-  const { name, email, phone } = body;
+  const { name, email, phone, x, y } = body;
 
-  const queryString = `INSERT INTO costumers(name, email, phone) 
-    VALUES ($1, $2, $3) 
+  const queryString = `INSERT INTO costumers(name, email, phone, x, y) 
+    VALUES ($1, $2, $3, $4, $5) 
     RETURNING *;`;  
 
-  const values = [name, email, phone];  
+  const values = [name, email, phone, x, y];  
 
   const response = await client.query<ICostumer>(queryString, values);
 

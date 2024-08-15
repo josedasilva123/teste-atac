ac2caa48717ca965c1b040231f5ba2fb6592b48a
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-15 16:55:02 -0300

    feat: migration script

---
+++ backend/src/database/migration.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import { Pool } from "pg";
+import { env } from "../env";
+
+export const migrate = async () => {
+  try {
+    console.log("--Migration started--");
+    const pool = new Pool({
+      user: env.DB_USER,
+      password: env.DB_PASSWORD,
+      host: env.DB_HOST,
+      port: Number(env.DB_PORT),
+      database: env.DB_DATABASE,
+    });
+
+    const client = await pool.connect();
+    await client.query(`CREATE TABLE IF NOT EXISTS costumers (
+        "id" SERIAL PRIMARY KEY,
+        "name" VARCHAR(50) NOT NULL,
+        "email" VARCHAR(100) NOT NULL,
+        "phone" VARCHAR(20) NOT NULL,
+        "x" INTEGER NOT NULL,
+        "y" INTEGER NOT NULL
+    );`);
+    await client.release();
+    console.log("--Migration sucessfully--");
+  } catch (error) {
+    console.log("--Migration failed--");
+    console.log(error);
+  }
+};
+
+migrate();

39cb5c6fc2ebb2046e7389dc5632566473f712c4
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-16 09:41:45 -0300

    feat: button base component

---
+++ frontend/src/components/shared/fragments/Button/index.tsx
@@ -0,0 +1,13 @@
+import { ButtonHTMLAttributes } from "react";
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement>{
+    children: React.ReactNode;
+}
+
+export function Button({ children, ...rest }: Props){
+    return(
+        <button {...rest}>
+            {children}
+        </button>
+    )
+}

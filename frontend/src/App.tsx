89b8b95704bbf6197f0ecf4f2bbce34b78cf9988
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2024-08-16 11:22:14 -0300

    feat: reset styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Template } from "./components/shared/template/Template";
 import { HomePage } from "./pages/HomePage";
+import "./styles/index.scss";
 
 const client = new QueryClient();
 
